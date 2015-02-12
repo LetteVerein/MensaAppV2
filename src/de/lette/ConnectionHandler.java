01b16936c7b9098784861d1f8fc90bbebc5ab672
DarkAtra <darkatra@web.de>
2015-02-12 22:41:01 +0100

    Mehr Dummy Daten für bessere Tests

--- src/de/lette/ConnectionHandler.java
+++ src/de/lette/ConnectionHandler.java
@@ -9,15 +9,16 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.http.client.ClientProtocolException;
 
-import android.text.format.Time;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -39,57 +40,64 @@ public class ConnectionHandler {
 		ClientData data = new ClientData();
 
 		Set<de.lette.mensaplan.server.Speise> alleSpeisen = new LinkedHashSet<de.lette.mensaplan.server.Speise>();
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(1, "Hamburger", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(2, "Caesar Salat", SpeiseArt.VORSPEISE,false, "Salat", 100, 100, 100, 100));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(3, "Suppe", SpeiseArt.VORSPEISE, false, "Suppe", 200, 200, 200, 200));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(4, "Pferde Steak", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(5, "Bio Burger", SpeiseArt.VOLLKOST, true, "Bio", 0, 0, 0, 0));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(6, "Eis im Eimer", SpeiseArt.DESSERT, false, "Eis", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(1, "Suppe", SpeiseArt.VORSPEISE, false, "Suppe", 200, 200, 200, 200));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(2, "Gurken Salat", SpeiseArt.VORSPEISE,false, "Salat", 100, 100, 100, 100));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(3, "Schwere Suppe", SpeiseArt.VORSPEISE, false, "Suppe", 200, 200, 200, 200));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(4, "Obelix Salat", SpeiseArt.VORSPEISE,false, "Salat", 100, 100, 100, 100));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(5, "Leichte Suppe", SpeiseArt.VORSPEISE, false, "Suppe", 200, 200, 200, 200));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(6, "Caesar Salat", SpeiseArt.VORSPEISE,false, "Salat", 100, 100, 100, 100));
 		
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(7, "Cheeseburger", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(8, "Obelix Salat", SpeiseArt.VORSPEISE,false, "Salat", 100, 100, 100, 100));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(9, "Leichte Suppe", SpeiseArt.VORSPEISE, false, "Suppe", 200, 200, 200, 200));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(10, "Kuh Steak", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(11, "Bio Banane", SpeiseArt.VOLLKOST, true, "Bio", 0, 0, 0, 0));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(12, "Eis im Becher", SpeiseArt.DESSERT, false, "Eis", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(7, "Hamburger", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(8, "Pferde Steak", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(9, "Bio Burger", SpeiseArt.VOLLKOST, true, "Bio", 0, 0, 0, 0));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(10, "Mc Chicken", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(11, "Kuh Fladen", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(12, "Bio Birne", SpeiseArt.VOLLKOST, true, "Bio", 0, 0, 0, 0));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(13, "Cheeseburger", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(14, "Kuh Steak", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(15, "Bio Banane", SpeiseArt.VOLLKOST, true, "Bio", 0, 0, 0, 0));
 		
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(13, "Mc Chicken", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(14, "Gurken Salat", SpeiseArt.VORSPEISE,false, "Salat", 100, 100, 100, 100));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(15, "Schwere Suppe", SpeiseArt.VORSPEISE, false, "Suppe", 200, 200, 200, 200));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(16, "Kuh Fladen", SpeiseArt.VOLLKOST, false, "Fleisch", 9000, 9000, 9000, 9000));
-		alleSpeisen.add(new de.lette.mensaplan.server.Speise(17, "Bio Birne", SpeiseArt.VOLLKOST, true, "Bio", 0, 0, 0, 0));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(16, "Eis im Becher", SpeiseArt.DESSERT, false, "Eis", 9000, 9000, 9000, 9000));
+		alleSpeisen.add(new de.lette.mensaplan.server.Speise(17, "Eis im Eimer", SpeiseArt.DESSERT, false, "Eis", 9000, 9000, 9000, 9000));
 		alleSpeisen.add(new de.lette.mensaplan.server.Speise(18, "Pudding im Becher", SpeiseArt.DESSERT, false, "Pudding", 9000, 9000, 9000, 9000));
 		data.setSpeisen(alleSpeisen);
 
 		Set<Termin> termine = new LinkedHashSet<Termin>();
-		Time today = new Time(Time.getCurrentTimezone());
-		Time tomorrow = new Time(Time.getCurrentTimezone());
-		Time nextWeek = new Time(Time.getCurrentTimezone());
-		today.setToNow();
-		tomorrow.setToNow();
-		nextWeek.setToNow();
-		tomorrow.monthDay += 1;
-		nextWeek.monthDay += 7;
-		termine.add(new Termin(1, new Date(today.toMillis(false)), 1, new BigDecimal(3.75), false));
-		termine.add(new Termin(2, new Date(today.toMillis(false)), 2, new BigDecimal(2.75), true));
-		termine.add(new Termin(3, new Date(today.toMillis(false)), 3, new BigDecimal(1.75), true));
-		termine.add(new Termin(4, new Date(today.toMillis(false)), 4, new BigDecimal(1.75), false));
-		termine.add(new Termin(5, new Date(today.toMillis(false)), 5, new BigDecimal(2.00), false));
-		termine.add(new Termin(6, new Date(today.toMillis(false)), 6, new BigDecimal(2.50), false));
+		Calendar c = Calendar.getInstance(Locale.getDefault());
+		c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
 		
-		termine.add(new Termin(7, new Date(tomorrow.toMillis(false)), 7, new BigDecimal(3.75), false));
-		termine.add(new Termin(8, new Date(tomorrow.toMillis(false)), 8, new BigDecimal(2.75), true));
-		termine.add(new Termin(9, new Date(tomorrow.toMillis(false)), 9, new BigDecimal(1.75), true));
-		termine.add(new Termin(10, new Date(tomorrow.toMillis(false)), 10, new BigDecimal(1.75), false));
-		termine.add(new Termin(11, new Date(tomorrow.toMillis(false)), 11, new BigDecimal(2.00), false));
-		termine.add(new Termin(12, new Date(tomorrow.toMillis(false)), 12, new BigDecimal(2.50), false));
+		// Monday This Week
+		termine.add(new Termin(1, new Date(c.getTimeInMillis()), 1, new BigDecimal(1.00), false));
+		termine.add(new Termin(2, new Date(c.getTimeInMillis()), 7, new BigDecimal(1.75), false));
+		termine.add(new Termin(3, new Date(c.getTimeInMillis()), 8, new BigDecimal(1.75), false));
+		termine.add(new Termin(4, new Date(c.getTimeInMillis()), 16, new BigDecimal(1.00), false));
 		
-		termine.add(new Termin(13, new Date(nextWeek.toMillis(false)), 13, new BigDecimal(3.75), false));
-		termine.add(new Termin(14, new Date(nextWeek.toMillis(false)), 14, new BigDecimal(2.75), true));
-		termine.add(new Termin(15, new Date(nextWeek.toMillis(false)), 15, new BigDecimal(1.75), true));
-		termine.add(new Termin(16, new Date(nextWeek.toMillis(false)), 16, new BigDecimal(1.75), false));
-		termine.add(new Termin(17, new Date(nextWeek.toMillis(false)), 17, new BigDecimal(2.00), false));
-		termine.add(new Termin(18, new Date(nextWeek.toMillis(false)), 18, new BigDecimal(2.50), false));
+		// Monday +1 ;D
+		c.add(Calendar.DAY_OF_WEEK, 1);
+		termine.add(new Termin(5, new Date(c.getTimeInMillis()), 2, new BigDecimal(2.00), false));
+		termine.add(new Termin(6, new Date(c.getTimeInMillis()), 9, new BigDecimal(2.50), false));
+		termine.add(new Termin(7, new Date(c.getTimeInMillis()), 17, new BigDecimal(3.75), false));
+
+		// Monday +2 ;D
+		c.add(Calendar.DAY_OF_WEEK, 1);
+		termine.add(new Termin(8, new Date(c.getTimeInMillis()), 3, new BigDecimal(2.75), false));
+		termine.add(new Termin(9, new Date(c.getTimeInMillis()), 10, new BigDecimal(1.75), false));
+		termine.add(new Termin(10, new Date(c.getTimeInMillis()), 11, new BigDecimal(1.75), false));
+		termine.add(new Termin(11, new Date(c.getTimeInMillis()), 18, new BigDecimal(2.00), false));
+
+		// Monday +3 ;D
+		c.add(Calendar.DAY_OF_WEEK, 1);
+		termine.add(new Termin(12, new Date(c.getTimeInMillis()), 4, new BigDecimal(2.50), false));
+		termine.add(new Termin(13, new Date(c.getTimeInMillis()), 12, new BigDecimal(3.75), false));
+		termine.add(new Termin(14, new Date(c.getTimeInMillis()), 13, new BigDecimal(2.75), false));
+		termine.add(new Termin(15, new Date(c.getTimeInMillis()), 16, new BigDecimal(1.75), false));
+		
+		// Monday +4 ;D
+		c.add(Calendar.DAY_OF_WEEK, 1);
+		termine.add(new Termin(16, new Date(c.getTimeInMillis()), 5, new BigDecimal(1.75), false));
+		termine.add(new Termin(17, new Date(c.getTimeInMillis()), 14, new BigDecimal(2.00), false));
+		termine.add(new Termin(18, new Date(c.getTimeInMillis()), 15, new BigDecimal(2.00), false));
+		termine.add(new Termin(19, new Date(c.getTimeInMillis()), 18, new BigDecimal(2.50), false));
 		data.setTermine(termine);
 
 		Set<Zusatzstoff> zusätze = new LinkedHashSet<Zusatzstoff>();
